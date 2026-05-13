dcf976ea6a2f060b15bda2edf673951eb50c2985
xsalefter <xsalefter@gmail.com>
2026-05-14 04:18:56 +0700

    migrate from javax to jakarta namespace

--- src/main/java/org/killbill/billing/plugin/gocardless/GoCardlessCheckoutServlet.java
+++ src/main/java/org/killbill/billing/plugin/gocardless/GoCardlessCheckoutServlet.java
@@ -34,8 +34,8 @@ import org.killbill.billing.util.callcontext.CallContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Named;
-import javax.inject.Singleton;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
 import java.util.Optional;
 import java.util.UUID;
 
